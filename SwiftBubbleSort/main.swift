391154f50541a2ebd356551a8c7a8ac2362efcbc
leny <leny.lopez19@gmail.com>
2024-03-27 10:26:26 -0400

    bubbleSort
    bubble sort in swift

--- SwiftBubbleSort/main.swift
+++ SwiftBubbleSort/main.swift
@@ -7,16 +7,15 @@
 
 import Foundation
 
-var numbers:[Int] = [5,4,3,2,1]
-var num:[Int] = [14,53,62,645,467,2,53,67,46,25,16,64,74,45,73,37,37,47,86,56]
-var swap:Int = 0;
+var test1:[Int] = [5,4,3,2,1]
+var test2:[Int] = [14,53,62,645,467,2,53,67,46,25,16,64,74,45,73,37,37,47,86,56]
 
 func bubleSort(to toSort: inout[Int] ){
     let length:Int = toSort.count;
     for i in 0..<length{
         for j in 0..<((length-1) - i){
             if(toSort[j] > toSort[j+1]){
-                swap = toSort[j]
+                let swap = toSort[j]
                 toSort[j] = toSort[j+1]
                 toSort[j+1] = swap
             }
@@ -24,6 +23,7 @@ func bubleSort(to toSort: inout[Int] ){
     }
 }
 
-//bubleSort(to: &numbers)
-bubleSort(to: &num)
-print(num)
+bubleSort(to: &test1)
+print(test1)
+bubleSort(to: &test2)
+print(test2)
